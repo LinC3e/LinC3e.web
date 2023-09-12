022a94dbba95eb6431be78b73e5e16388617817a
LinC3e <ivancostilla17@gmail.com>
2023-09-12 18:07:45 -0300

    ADD - Animation headerSection

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,5 +1,7 @@
+"use client"
 import React from "react";
 import Image from "next/image";
+import { TypeAnimation } from 'react-type-animation';
 
 const HeroSection = () => {
     return (
@@ -7,6 +9,22 @@ const HeroSection = () => {
             <div className="grid grid-cols-1 sm:grid-cols-12">
                 <div className="col-span-7 place-self-center text-center sm:text-left">
                     <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-bold'>LinC3e</h1>
+                    <TypeAnimation
+                        sequence={[
+                            // Same substring at the start will only be typed out once, initially
+                            "Backend Developer",
+                            1000,
+                            'Web Developer',
+                            1000,
+                            'From Argentina',
+                            1000
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        className= "text-red-400 mb-2 font-semibold"
+                        style={{ fontSize: '2em', display: 'inline-block' }}
+                        repeat={Infinity}
+                    />
                     <p className="text-[#ADB7BE] text-lg lg:text-xl mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, quibusdam.</p>
                     <div>
                         <button className="px-6 py-3 mb-4 w-56 sm:w-fit rounded-full bg-red-700 mr-4 hover:bg-slate-200 hover:text-black">Hire me</button>
@@ -16,7 +34,7 @@ const HeroSection = () => {
                 <div className="col-span-5 place-self-center mt-4 lg:mt-0">
                     <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
                         <Image
-                            src ="/images/hero-ber.jpg"
+                            src="/images/hero-ber.jpg"
                             alt="hero image"
                             className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                             width={400}
