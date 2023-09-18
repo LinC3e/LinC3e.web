7875040d8b88a49863a9307c096b9fab508e6d58
LinC3e <ivancostilla17@gmail.com>
2023-09-18 17:37:49 -0300

    ADD - menu responsive

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import React, { useState } from 'react';
 import NavLink from './NavLink';
 import { Bars3Icon, XmarkIcon } from "@heroicons/react/24/solid";
+import Menu from './Menu';
 
 const navLinks = [
     {
@@ -23,7 +24,7 @@ const NavBar = () => {
     const [navbarOpen, setNavbarOpen] = useState(false);
 
     return (
-        <nav className='fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
+        <nav className='fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
             <div className='flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2'>
                 <Link href={"/"} className='text-3xl text-white font-semibold'>LOGO</Link>
                 <div className='mobile-menu block md:hidden'>
@@ -51,6 +52,7 @@ const NavBar = () => {
                     </ul>
                 </div>
             </div>
+            {navbarOpen ? <Menu links={navLinks} /> : null}
         </nav>
     )
 }
