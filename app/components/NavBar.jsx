12ca48b6494dd1f4ede57ef70173379d422d7ca0
LinC3e <ivancostilla17@gmail.com>
2023-09-18 17:31:39 -0300

    ADD - Hero icons for the menu, smallwin

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,6 +1,8 @@
-import Link from 'next/link'
-import React from 'react'
-import NavLink from './NavLink'
+"use client";
+import Link from 'next/link';
+import React, { useState } from 'react';
+import NavLink from './NavLink';
+import { Bars3Icon, XmarkIcon } from "@heroicons/react/24/solid";
 
 const navLinks = [
     {
@@ -18,16 +20,31 @@ const navLinks = [
 ];
 
 const NavBar = () => {
-    return(
+    const [navbarOpen, setNavbarOpen] = useState(false);
+
+    return (
         <nav className='fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
             <div className='flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2'>
                 <Link href={"/"} className='text-3xl text-white font-semibold'>LOGO</Link>
+                <div className='mobile-menu block md:hidden'>
+                    {
+                        ! navbarOpen ? (
+                            <button onClick={() => setNavbarOpen(true)} className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-orange-600 hover:border-orange-600'>
+                                <Bars3Icon className='h-5 w-5' />
+                            </button>
+                        ) : (
+                            <button onClick={() => setNavbarOpen(false)} className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-orange-600 hover:border-orange-600'>
+                                <XmarkIcon className='h-5 w-5' />
+                            </button>
+                        )
+                    }
+                </div>
                 <div className="menu hidden md:block md:w-auto" id="navbar">
                     <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
                         {
                             navLinks.map((link, index) => (
                                 <li key={index}>
-                                    <NavLink href={link.path} title={link.title}/>
+                                    <NavLink href={link.path} title={link.title} />
                                 </li>
                             ))
                         }
